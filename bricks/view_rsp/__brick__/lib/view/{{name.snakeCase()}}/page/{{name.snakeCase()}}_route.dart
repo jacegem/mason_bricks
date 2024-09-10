0a03da91a6e282cb0fde7c182fa3b1037de8f24d
JACE <jacegem@gmail.com>
2024-09-11 08:27:51 +0900

    fix data_isar

--- bricks/view_rsp/__brick__/lib/view/{{name.snakeCase()}}/page/{{name.snakeCase()}}_route.dart
+++ bricks/view_rsp/__brick__/lib/view/{{name.snakeCase()}}/page/{{name.snakeCase()}}_route.dart
@@ -1,11 +1,12 @@
-import 'package:fabric_mask/view/setting/page/contact_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import '{{name.snakeCase()}}_screen.dart';
+
 
 class {{name.pascalCase()}}Route extends GoRouteData {
   const {{name.pascalCase()}}Route();
 
-  static const path = '{{name.pascalCase()}}';
+  static const path = '{{name.snakeCase()}}';
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
